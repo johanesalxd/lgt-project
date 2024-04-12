c0e3036d36b0643494b9a2f4f83a94f8c7f25f20
Jo Alex <17249308+johanesalxd@users.noreply.github.com>
2024-04-12 23:49:02 +0800

    add method for add

--- maps/dictionary.go
+++ maps/dictionary.go
@@ -6,12 +6,16 @@ var ErrNotFound = errors.New("could not find the word you were looking for")
 
 type Dictionary map[string]string
 
-func (d Dictionary) Search(keys string) (string, error) {
-	val, ok := d[keys]
+func (d Dictionary) Search(key string) (string, error) {
+	val, ok := d[key]
 
 	if !ok {
 		return "", ErrNotFound
 	}
 
 	return val, nil
+}
+
+func (d Dictionary) Add(key, value string) {
+	d[key] = value
 }
--- maps/dictionary_test.go
+++ maps/dictionary_test.go
@@ -22,6 +22,16 @@ func TestSearch(t *testing.T) {
 	})
 }
 
+func TestAdd(t *testing.T) {
+	dict := Dictionary{}
+	k := "test"
+	v := "this is just a test"
+
+	dict.Add(k, v)
+
+	assertDefinition(t, dict, k, v)
+}
+
 func assertStrings(t testing.TB, got, want string) {
 	t.Helper()
 
@@ -36,4 +46,16 @@ func assertError(t testing.TB, got, want error) {
 	if got != want {
 		t.Errorf("ogt %q want %q", got, want)
 	}
+}
+
+func assertDefinition(t testing.TB, dict Dictionary, k, v string) {
+	t.Helper()
+
+	got, err := dict.Search(k)
+
+	if err != nil {
+		t.Fatal("shold find added word: ", err)
+	}
+
+	assertStrings(t, got, v)
 }
