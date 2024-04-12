73964ea772a3cfdc70ca8eb9f41e677e96b7b08f
Jo Alex <17249308+johanesalxd@users.noreply.github.com>
2024-04-12 16:01:47 +0800

    adding constant, refactor test

--- hello_test.go
+++ hello_test.go
@@ -3,8 +3,22 @@ package main
 import "testing"
 
 func TestHello(t *testing.T) {
-	got := Hello("Chris")
-	want := "Hello, Chris"
+	t.Run("saying hello to people", func(t *testing.T) {
+		got := Hello("Chris")
+		want := "Hello, Chris"
+
+		assetCorrectMessage(t, got, want)
+	})
+	t.Run("use 'world' on empty string input", func(t *testing.T) {
+		got := Hello("")
+		want := "Hello, world"
+
+		assetCorrectMessage(t, got, want)
+	})
+}
+
+func assetCorrectMessage(t testing.TB, got, want string) {
+	t.Helper()
 
 	if got != want {
 		t.Errorf("got %q want %q", got, want)
