ce7847347dead23ea77b47f4be32a32c0def852c
Jo Alex <17249308+johanesalxd@users.noreply.github.com>
2024-04-12 16:52:26 +0800

    add benchmark and example on iteration package

--- iteration/repeat.go
+++ iteration/repeat.go
@@ -1,10 +1,10 @@
 package iteration
 
-func Repeat(char string) string {
+func Repeat(char string, count int) string {
 	var repeated string
 
-	for i := 0; i < 5; i++ {
-		repeated = repeated + char
+	for i := 0; i < count; i++ {
+		repeated += char
 	}
 
 	return repeated
--- iteration/repeat_test.go
+++ iteration/repeat_test.go
@@ -1,12 +1,27 @@
 package iteration
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+)
 
 func TestRepeat(t *testing.T) {
-	got := Repeat("a")
+	got := Repeat("a", 5)
 	want := "aaaaa"
 
 	if got != want {
 		t.Errorf("got %q want %q", got, want)
 	}
+}
+
+func BenchmarkRepeat(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		Repeat("a", 5)
+	}
+}
+
+func ExampleRepeat() {
+	sum := Repeat("x", 3)
+	fmt.Println(sum)
+	// Output: xxx
 }
