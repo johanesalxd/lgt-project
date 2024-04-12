158d104c711e1fc09872fc8b4b097fd096bfa332
Jo Alex <17249308+johanesalxd@users.noreply.github.com>
2024-04-12 22:01:50 +0800

    add method for area

--- structs/shapes.go
+++ structs/shapes.go
@@ -1,9 +1,28 @@
 package structs
 
-func Perimeter(width, height float64) float64 {
-	return 2 * (width + height)
+import "math"
+
+type Circle struct {
+	Radius float64
 }
 
-func Area(width, height float64) float64 {
-	return width * height
+func (c Circle) Area() float64 {
+	return math.Pi * c.Radius * c.Radius
 }
+
+type Rectangle struct {
+	Width  float64
+	Height float64
+}
+
+func (r Rectangle) Area() float64 {
+	return r.Width * r.Height
+}
+
+func Perimeter(rectangle Rectangle) float64 {
+	return 2 * (rectangle.Width + rectangle.Height)
+}
+
+// func Area(rectangle Rectangle) float64 {
+// 	return rectangle.Width * rectangle.Height
+// }
