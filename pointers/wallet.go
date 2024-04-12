029a09f584970932e3e2d9ae164e88798b7a5da8
Jo Alex <17249308+johanesalxd@users.noreply.github.com>
2024-04-12 22:56:44 +0800

    adding return error and assertion

--- pointers/wallet.go
+++ pointers/wallet.go
@@ -1,6 +1,9 @@
 package pointers
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+)
 
 type Bitcoin int
 
@@ -22,4 +25,14 @@ func (w *Wallet) Deposit(amount Bitcoin) {
 
 func (w *Wallet) Balance() Bitcoin {
 	return w.balance
+}
+
+func (w *Wallet) Withdraw(amount Bitcoin) error {
+	if amount > w.balance {
+		return errors.New("oh no")
+	}
+
+	w.balance -= amount
+
+	return nil
 }
