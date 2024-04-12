62e2ef0a55f34589e5e033d126a509b2a2616a49
Jo Alex <17249308+johanesalxd@users.noreply.github.com>
2024-04-12 22:09:22 +0800

    adding interface

--- structs/shapes.go
+++ structs/shapes.go
@@ -19,10 +19,10 @@ func (r Rectangle) Area() float64 {
 	return r.Width * r.Height
 }
 
+type Shape interface {
+	Area() float64
+}
+
 func Perimeter(rectangle Rectangle) float64 {
 	return 2 * (rectangle.Width + rectangle.Height)
 }
-
-// func Area(rectangle Rectangle) float64 {
-// 	return rectangle.Width * rectangle.Height
-// }
