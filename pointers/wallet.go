8c2936b8009cd50fd0e6a9fc1735d2301d61aebe
Jo Alex <17249308+johanesalxd@users.noreply.github.com>
2024-04-12 23:09:24 +0800

    refactor tests

--- pointers/wallet.go
+++ pointers/wallet.go
@@ -5,6 +5,8 @@ import (
 	"fmt"
 )
 
+var ErrInsufficientFunds = errors.New("cannot withdraw, insufficient funds")
+
 type Bitcoin int
 
 func (b Bitcoin) String() string {
@@ -29,7 +31,7 @@ func (w *Wallet) Balance() Bitcoin {
 
 func (w *Wallet) Withdraw(amount Bitcoin) error {
 	if amount > w.balance {
-		return errors.New("oh no")
+		return ErrInsufficientFunds
 	}
 
 	w.balance -= amount
