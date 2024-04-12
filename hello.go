129c1c8a5f492747931c5ba0da56495759c0a008
Jo Alex <17249308+johanesalxd@users.noreply.github.com>
2024-04-12 16:09:42 +0800

    adding spanish and french

--- hello.go
+++ hello.go
@@ -2,16 +2,26 @@ package main
 
 import "fmt"
 
-const englishHelloPrefix = "Hello, "
+const (
+	englishHelloPrefix = "Hello, "
+	spanishHelloPrefix = "Hola, "
+	frenchHelloPrefix  = "Bonjour, "
+)
 
-func Hello(name string) string {
+func Hello(name, language string) string {
 	if name == "" {
 		name = "world"
+	}
+	if language == "Spanish" {
+		return spanishHelloPrefix + name
+	}
+	if language == "French" {
+		return frenchHelloPrefix + name
 	}
 
 	return englishHelloPrefix + name
 }
 
 func main() {
-	fmt.Println(Hello("world"))
+	fmt.Println(Hello("world", ""))
 }
--- hello_test.go
+++ hello_test.go
@@ -4,14 +4,26 @@ import "testing"
 
 func TestHello(t *testing.T) {
 	t.Run("saying hello to people", func(t *testing.T) {
-		got := Hello("Chris")
+		got := Hello("Chris", "")
 		want := "Hello, Chris"
 
 		assetCorrectMessage(t, got, want)
 	})
 	t.Run("use 'world' on empty string input", func(t *testing.T) {
-		got := Hello("")
+		got := Hello("", "")
 		want := "Hello, world"
+
+		assetCorrectMessage(t, got, want)
+	})
+	t.Run("saying hello in Spanish", func(t *testing.T) {
+		got := Hello("Elodie", "Spanish")
+		want := "Hola, Elodie"
+
+		assetCorrectMessage(t, got, want)
+	})
+	t.Run("saying hello in French", func(t *testing.T) {
+		got := Hello("Elodie", "French")
+		want := "Bonjour, Elodie"
 
 		assetCorrectMessage(t, got, want)
 	})
