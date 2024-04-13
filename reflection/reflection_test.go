26f893222404b2e0b83b30966e67683479f5ed9e
Jo Alex <17249308+johanesalxd@users.noreply.github.com>
2024-04-13 21:33:17 +0800

    add support for other types and refactor code

--- reflection/reflection_test.go
+++ reflection/reflection_test.go
@@ -71,6 +71,14 @@ func TestWalk(t *testing.T) {
 			},
 			ExpectedCalls: []string{"London", "Paris"},
 		},
+		{
+			Name: "maps",
+			Input: map[string]string{
+				"Cow":   "Moo",
+				"Sheep": "Baa",
+			},
+			ExpectedCalls: []string{"Moo", "Baa"},
+		},
 	}
 
 	for _, test := range cases {
