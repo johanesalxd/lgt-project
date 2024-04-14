30ae8ae491aa6fcf34aa2ebec78c27d87f9f0675
Jo Alex <17249308+johanesalxd@users.noreply.github.com>
2024-04-14 12:37:46 +0800

    add read posts implementation

--- reading_files/blogposts.go
+++ reading_files/blogposts.go
@@ -1,8 +1,8 @@
 package readingfiles
 
 import (
+	"io"
 	"io/fs"
-	"testing/fstest"
 )
 
 type Post struct {
@@ -10,14 +10,39 @@ type Post struct {
 	Tags                     []string
 }
 
-func NewPostsFromFS(inputFs fstest.MapFS) []Post {
-	var posts []Post
-
-	dir, _ := fs.ReadDir(inputFs, ".")
-
-	for range dir {
-		posts = append(posts, Post{})
+func NewPostsFromFS(inputFS fs.FS) ([]Post, error) {
+	dir, err := fs.ReadDir(inputFS, ".")
+	if err != nil {
+		return nil, err
 	}
 
-	return posts
+	var posts []Post
+
+	for _, f := range dir {
+		post, err := getPost(inputFS, f)
+		if err != nil {
+			return nil, err
+		}
+
+		posts = append(posts, post)
+	}
+
+	return posts, nil
+}
+
+func getPost(inputFS fs.FS, f fs.DirEntry) (Post, error) {
+	postFile, err := inputFS.Open(f.Name())
+	if err != nil {
+		return Post{}, err
+	}
+	defer postFile.Close()
+
+	postData, err := io.ReadAll(postFile)
+	if err != nil {
+		return Post{}, err
+	}
+
+	post := Post{Title: string(postData[7:])}
+
+	return post, nil
 }
--- reading_files/blogposts_test.go
+++ reading_files/blogposts_test.go
@@ -1,24 +1,50 @@
 package readingfiles_test
 
 import (
+	"errors"
+	"io/fs"
+	"reflect"
 	"testing"
 	"testing/fstest"
 
 	readingfiles "github.com/johanesalxd/lgt-project/reading_files"
 )
 
+type StubFailingFS struct{}
+
+func (s StubFailingFS) Open(name string) (fs.File, error) {
+	return nil, errors.New("always fail")
+}
+
 func TestNewBlogPosts(t *testing.T) {
-	fs := fstest.MapFS{
-		"hello_world.md":   {Data: []byte("hi")},
-		"hello_world_2.md": {Data: []byte("hola")},
-	}
+	t.Run("error handling test", func(t *testing.T) {
+		_, err := readingfiles.NewPostsFromFS(StubFailingFS{})
 
-	posts := readingfiles.NewPostsFromFS(fs)
+		if err == nil {
+			t.Fatal(err)
+		}
+	})
+	t.Run("content and file checking", func(t *testing.T) {
+		fs := fstest.MapFS{
+			"hello_world.md":   {Data: []byte("Title: Post 1")},
+			"hello_world_2.md": {Data: []byte("Title: Post 2")},
+		}
 
-	got := len(posts)
-	want := len(fs)
+		posts, err := readingfiles.NewPostsFromFS(fs)
 
-	if got != want {
-		t.Errorf("got %d want %d", got, want)
-	}
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		got := posts[0]
+		want := readingfiles.Post{Title: "Post 1"}
+
+		if !reflect.DeepEqual(got, want) {
+			t.Errorf("got %+v want %+v", got, want)
+		}
+
+		if len(posts) != len(fs) {
+			t.Errorf("got %d want %d", len(posts), len(fs))
+		}
+	})
 }
