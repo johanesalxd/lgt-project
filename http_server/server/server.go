6b334aff0c0d994e6fd23785b135df35a550ea44
Jo Alex <17249308+johanesalxd@users.noreply.github.com>
2024-04-14 14:52:27 +0800

    add simple player implementation

--- http_server/server/server.go
+++ http_server/server/server.go
@@ -3,8 +3,18 @@ package server
 import (
 	"fmt"
 	"net/http"
+	"strings"
 )
 
 func PlayerServer(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "20")
+	player := strings.TrimPrefix(r.URL.Path, "/players/")
+
+	switch player {
+	case "Pepper":
+		fmt.Fprint(w, "20")
+		return
+	case "Floyd":
+		fmt.Fprint(w, "10")
+		return
+	}
 }
