eda0796f931ff83b52b6f4610cc3ffa7973ab95f
Jo Alex <17249308+johanesalxd@users.noreply.github.com>
2024-04-15 08:15:39 +0800

    refactor code

--- http_server/server/server.go
+++ http_server/server/server.go
@@ -16,17 +16,18 @@ type PlayerServer struct {
 }
 
 func (p *PlayerServer) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	player := strings.TrimPrefix(r.URL.Path, "/players/")
+
 	switch r.Method {
 	case http.MethodGet:
-		p.getScore(w, r)
+		p.showScore(w, player)
 	case http.MethodPost:
-		p.processWin(w, r)
+		p.processWin(w, player)
 	}
 
 }
 
-func (p *PlayerServer) getScore(w http.ResponseWriter, r *http.Request) {
-	player := strings.TrimPrefix(r.URL.Path, "/players/")
+func (p *PlayerServer) showScore(w http.ResponseWriter, player string) {
 	score := p.Store.GetPlayerScore(player)
 
 	if score == 0 {
@@ -36,8 +37,7 @@ func (p *PlayerServer) getScore(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(w, score)
 }
 
-func (p *PlayerServer) processWin(w http.ResponseWriter, r *http.Request) {
-	player := strings.TrimPrefix(r.URL.Path, "/players/")
+func (p *PlayerServer) processWin(w http.ResponseWriter, player string) {
 	p.Store.RecordWin(player)
 
 	w.WriteHeader(http.StatusAccepted)
